d956ba0af20be7e55b2cf6e63f0fb4d8fe030ee1
Malia Wakesho-Ajwang <maz.obiri@emory.edu>
2025-08-30 14:11:51 +0200

    updating config files to fix web deployment

--- app.config.js
+++ app.config.js
@@ -10,5 +10,10 @@ export default {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     },
+    platforms: ['ios', 'android', 'web'],
+    web: {
+      bundler: 'metro',
+      output: 'static',
+    },
   },
 };
